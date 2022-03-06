27c79147cd5ec50df70b625911b7306682deb8e8
Blue <leanefelis@gmail.com>
2022-03-06 18:04:22 +0100

    hop :cd: :musical_note:

---
+++ Jeux2/src/main/java/module-info.java
@@ -0,0 +1,11 @@
+module hello.jeux {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+    requires org.controlsfx.controls;
+    requires com.dlsc.formsfx;
+    requires org.kordamp.bootstrapfx.core;
+
+    opens menu to javafx.fxml;
+    exports menu;
+}
